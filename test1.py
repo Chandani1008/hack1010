8191f31b27609333f69641d1283d2904b387f686
singhchandani1008@gmail.com <singhchandani1008@gmail.com>
2019-10-11 21:34:37 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,12 @@
+#***********print these following lines.************
+#this is \\ double backslash
+#this is /\/\/\/\ mountain
+# he is awesome(use escape sequence instead of manual spaces)
+# \" \n \t \' (print this as an output)
+
+print("this is \\\\ double backslash")
+print("These are /\\/\\/\\/\\ mountain")
+print("he is \tawesome")
+print("\\\" \\n \\t \\\'")  #print(r"\" \n \t \'") also print the same 
+print(r"\" \n \t \'")
+
